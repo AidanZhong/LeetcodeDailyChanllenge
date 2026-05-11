17cd494ae713e4b5c3ff769d20a4f0e8acb5bbdb
50601 <506017129@qq.com>
2026-05-11 20:57:03 +0100

    Add implementation for 1665. Minimum Initial Energy to Finish Tasks with greedy approach and complexity analysis

--- code/1665_minimum_initial_energy_to_finish_tasks.py
+++ code/1665_minimum_initial_energy_to_finish_tasks.py
@@ -2,8 +2,35 @@
 # Topic: Greedy
 # Difficulty: Hard
 
+'''
+You are given an array tasks where tasks[i] = [actuali, minimumi]:
+
+actuali is the actual amount of energy you spend to finish the ith task.
+minimumi is the minimum amount of energy you require to begin the ith task.
+For example, if the task is [10, 12] and your current energy is 11, you cannot start this task. However, if your current energy is 13, you can complete this task, and your energy will be 3 after finishing it.
+
+You can finish the tasks in any order you like.
+
+Return the minimum initial amount of energy you will need to finish all the tasks.
+'''
+from typing import List
+
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        sorted_tasks = sorted(tasks, key=lambda x: x[1] - x[0], reverse=True)
+        minimum_energy = 0
+        current_energy = 0
+        for task in sorted_tasks:
+            if current_energy < task[1]:
+                offset = task[1] - current_energy
+                minimum_energy += offset
+                current_energy = task[1] - task[0]  # after consume
+            else:
+                current_energy -= task[0]
+        return minimum_energy
+
+
+# print(Solution().minimumEffort(tasks=[[1, 2], [2, 4], [4, 8]]))
+# print(Solution().minimumEffort(tasks=[[1, 3], [2, 4], [10, 11], [10, 12], [8, 9]]))
+print(Solution().minimumEffort(tasks=[[1, 2], [1, 7], [2, 3], [5, 9], [2, 2]]))
--- solutions/1665_minimum_initial_energy_to_finish_tasks.md
+++ solutions/1665_minimum_initial_energy_to_finish_tasks.md
@@ -3,14 +3,29 @@
 - **Difficulty:** Hard
 - **Topic:** Greedy
 
+https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/description/
+
 ## Approach
 
+Though they tagged the problem as hard, it's actually quite easy to solve if you think about it.
 
+The sequence of task manipulation is the key. Some task need a lot of energy to start but actually cost very little
+energy to finish. Some of them need the same amount of energy to start and finish. We call this the "Energy gap"
+
+Since we need the minimum energy to finish all tasks, we want the minimum energy left after finishing all tasks. 
+That is to say we want the task with the minimum energy gap to be done last. Try to do the task with the maximum
+energy gap first so we can have more remaining energy for the remaining tasks instead of wasting them.
+
+Then the problem becomes a simple greedy problem. All we need is sort the tasks array by the energy gap. Then simulate the process
+and keep track of the needed energy.
 
 ## Complexity
 
-- **Time:**
-- **Space:**
+- **Time:** O(n log n)
+  - Sorting tasks by energy gap: O(n log n)
+  - Single pass through sorted tasks: O(n)
+- **Space:** O(n)
+  - `sorted_tasks` stores a new sorted copy of the input array
 
 ## Code
 
