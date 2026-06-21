690a93212b7d23fa8c29897d4ee7708d64a6d661
50601 <506017129@qq.com>
2026-06-21 18:23:27 +0100

    Implement solution for 1189. Maximum Number of Balloons

--- code/1189_maximum_number_of_balloons.py
+++ code/1189_maximum_number_of_balloons.py
@@ -2,8 +2,29 @@
 # Topic: hash set
 # Difficulty: easy
 
+'''
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+
+
+Example 1:
+
+
+
+Input: text = "nlaebolko"
+Output: 1
+'''
+from collections import defaultdict
+
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def maxNumberOfBalloons(self, text: str) -> int:
+        char_dict = defaultdict(int)
+
+        for i in text:
+            if i in "balloon":
+                char_dict[i] += 1
+        ans = min(char_dict["b"], char_dict["a"], char_dict["l"] // 2, char_dict["o"] // 2, char_dict["n"])
+        return ans
--- solutions/1189_maximum_number_of_balloons.md
+++ solutions/1189_maximum_number_of_balloons.md
@@ -5,7 +5,9 @@
 
 ## Approach
 
-
+count the number of each character in the string. A balloon is 1 'a', 1 'b', 2 'l', 2 'o' and 1 'n'.
+Then count the total number of the text of each character. And use them to divide the amount of same character in the "
+balloon". The answer is the minimum of all the divisions.
 
 ## Complexity
 
