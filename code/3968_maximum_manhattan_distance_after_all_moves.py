e66a23a5cdf037c5f57bd7811058817dc0c405ca
50601 <506017129@qq.com>
2026-06-22 15:18:01 +0100

    Implement solution

--- code/3968_maximum_manhattan_distance_after_all_moves.py
+++ code/3968_maximum_manhattan_distance_after_all_moves.py
@@ -1,9 +1,33 @@
 # Problem: Maximum Manhattan Distance After All Moves
 # Topic: simulation
 # Difficulty: Medium
+'''
+You are given a string moves consisting of the characters 'U', 'D', 'L', 'R', and '_'.
+
+Starting from the origin (0, 0), each character represents one move on a 2D plane:
+
+'U': Move up by 1 unit.
+'D': Move down by 1 unit.
+'L': Move left by 1 unit.
+'R': Move right by 1 unit.
+'_': Can be independently replaced with any one of 'U', 'D', 'L', or 'R'.
+Return the maximum Manhattan distance from the origin that can be achieved after all moves have been performed.
+'''
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def maxDistance(self, moves: str) -> int:
+        x, y = 0, 0
+        count = 0
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'L':
+                x -= 1
+            elif move == 'R':
+                x += 1
+            else:
+                count += 1
+        return abs(x) + abs(y) + count
--- solutions/3968_maximum_manhattan_distance_after_all_moves.md
+++ solutions/3968_maximum_manhattan_distance_after_all_moves.md
@@ -5,7 +5,10 @@
 
 ## Approach
 
+No matter how you moved, the "_" move can only increase the Manhattan distance by 1 and can always be done.
 
+So what we do is to simulate the moves and count the _ move. Calculate the Manhattan distance after all moves and plus
+the number of "_" move.
 
 ## Complexity
 
