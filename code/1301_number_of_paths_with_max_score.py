8f38aac006e2e6af6258a3c1a126b71591fe3a96
50601 <506017129@qq.com>
2026-07-04 23:13:55 +0100

    Update approach and implement solution for 1301. Number of Paths with Max Score

--- code/1301_number_of_paths_with_max_score.py
+++ code/1301_number_of_paths_with_max_score.py
@@ -2,8 +2,77 @@
 # Topic: Dynamic programming
 # Difficulty: Hard
 
+'''
+You are given a square board of characters. You can move on the board starting at the bottom right square marked with the character 'S'.
+
+You need to reach the top left square marked with the character 'E'. The rest of the squares are labeled either with a numeric character 1, 2, ..., 9 or with an obstacle 'X'. In one move you can go up, left or up-left (diagonally) only if there is no obstacle there.
+
+Return a list of two integers: the first integer is the maximum sum of numeric characters you can collect, and the second is the number of such paths that you can take to get that maximum sum, taken modulo 10^9 + 7.
+
+In case there is no path, return [0, 0].
+
+
+
+Example 1:
+
+Input: board = ["E23","2X2","12S"]
+Output: [7,1]
+Example 2:
+
+Input: board = ["E12","1X1","21S"]
+Output: [4,2]
+Example 3:
+
+Input: board = ["E11","XXX","11S"]
+Output: [0,0]
+
+
+Constraints:
+
+2 <= board.length == board[i].length <= 100
+'''
+
+from functools import cache
+from typing import List
+
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        n = len(board)
+        MOD = 10 ** 9 + 7
+        board[n - 1] = board[n - 1].replace('S', '0')
+
+        @cache
+        def dp(i, j):
+            '''
+            Calculate the max score and number of paths from cell (i, j)
+            :param i: row
+            :param j: col
+            :return: max score, number of paths
+            '''
+            if i < 0 or j < 0 or i >= n or j >= n:
+                return [0, 0]  # no path
+
+            if (i, j) == (0, 0):
+                return [0, 1]
+            if board[i][j] == 'X':
+                return [0, 0]  # no path
+
+            adj = [dp(i - 1, j), dp(i, j - 1), dp(i - 1, j - 1)]
+            adj.sort(reverse=True)
+
+            if adj[0][0] > adj[1][0] >= adj[2][0]:
+                return [adj[0][0] + int(board[i][j]), adj[0][1] % MOD] if adj[0][1] != 0 else [0, 0]
+            elif adj[0][0] == adj[1][0] > adj[2][0]:
+                return [adj[0][0] + int(board[i][j]), (adj[0][1] + adj[1][1]) % MOD] if adj[0][1] != 0 else [0, 0]
+            else:
+                return [adj[0][0] + int(board[i][j]), (adj[0][1] + adj[1][1] + adj[2][1]) % MOD] if adj[0][1] != 0 else [0, 0]
+
+        # for i in range(n):
+        #     for j in range(n):
+        #         print(f'dp({i}, {j}) = {dp(i, j)}')
+        return dp(n - 1, n - 1)
+
+
+print(Solution().pathsWithMaxScore(["E23", "2X2", "12S"]))
+# print(Solution().pathsWithMaxScore(["E11", "XXX", "11S"]))
--- solutions/1301_number_of_paths_with_max_score.md
+++ solutions/1301_number_of_paths_with_max_score.md
@@ -7,12 +7,33 @@
 
 ## Approach
 
+Since we can only move towards up, left or up-left. So we cannot go back to each cell. Which means we could use dynamic programming to record any status of a cell.
+
+Define $dp(i, j)$ is the largest score of the path and the number of cell on $i$th row, $j$th col. It will return a tuple with max score call it $s(i, j)$ and the number of path call it $n(i, j)$. $dp(i, j)$ is determined by the cell next to it which is reachable. i.e. $dp(i+1, j), dp(i, j+1), dp(i+1, j+1)$. We call it set $dp\_next$. And we call the one with largest score $dp(x, y)$.
+
+If one of them got larger score than other two, It means we could only go that way to get maximum score.
+
+$$s(i, j) = s(x, y) + score(i, j)$$
+$$n(i, j) = n(x, y) $$
+
+If two of them are the same and larger than other one. It means we could go both way to get maximum score.
+
+$$s(i, j) = s(x_1, y_1) + score(i, j)$$
+$$n(i, j) = n(x_1, y_1) + n(x_2, y_2)$$
+
+If three of them are the same. It means we could go either of them.
+$$s(i, j) = s(x_1, y_1) + score(i, j)$$
+$$n(i, j) = n(x_1, y_1) + n(x_2, y_2) + n(x_3, y_3)$$
+
+Now let us consider the borders.
+$$ dp(0, 0) = 1, 0 $$
+$$ dp(i, j) = 0, 0 (board[i][j] == 'X') $$
 
 
 ## Complexity
 
-- **Time:**
-- **Space:**
+- **Time:** $O(n^2)$, each of the $n^2$ cells is computed once, doing $O(1)$ work (sorting 3 fixed-size tuples).
+- **Space:** $O(n^2)$ for the memoization cache, plus $O(n)$ recursion stack depth.
 
 ## Code
 
