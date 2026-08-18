979e7cbc0d6cbfc1e2ed12e0b9807a9bf0f04961
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-08-18 10:50:55 +0100

    Add 1563. Implement solution for Stone Game V and 3702. Implement solution for Longest Subsequence With Non-Zero Bitwise XOR

--- code/1563_stone_game_v.py
+++ code/1563_stone_game_v.py
@@ -1,9 +1,34 @@
 # Problem: Stone Game V
 # Topic: dynamic programming
 # Difficulty: Hard
+from functools import cache
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def stoneGameV(self, stoneValue: List[int]) -> int:
+
+        @cache
+        def dp(l, r):
+            # max value alice can get from stoneValue[l:r]
+            if l == r:
+                # boarder case, only 1 stone left
+                return 0
+            ans = left_sum = 0
+            total = sum(stoneValue[l: r + 1])
+            for x in range(l, r):
+                # try to split at position x
+                left_sum += stoneValue[x]
+                right_sum = total - left_sum
+                if left_sum > right_sum:
+                    # bob will throw left part
+                    ans = max(ans, right_sum + dp(x + 1, r))
+                elif left_sum < right_sum:
+                    # bob will throw right part
+                    ans = max(ans, left_sum + dp(l, x))
+                else:
+                    # bob will throw either part
+                    ans = max(ans, left_sum + dp(l, x), right_sum + dp(x + 1, r))
+            return ans
+
+        return dp(0, len(stoneValue) - 1)
--- code/3702_longest_subsequence_with_non-zero_bitwise_xor.py
+++ code/3702_longest_subsequence_with_non-zero_bitwise_xor.py
@@ -1,9 +1,22 @@
 # Problem: Longest Subsequence With Non-Zero Bitwise XOR
 # Topic: Bit manipulation
 # Difficulty: Medium
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def longestSubsequence(self, nums: List[int]) -> int:
+        n = len(nums)
+        contains_non_zero = False
+        xor_total = 0
+        for i in nums:
+            if i != 0:
+                contains_non_zero = True
+                xor_total ^= i
+
+        if xor_total != 0:
+            return n
+        elif contains_non_zero:
+            return n - 1
+        else:
+            return 0
