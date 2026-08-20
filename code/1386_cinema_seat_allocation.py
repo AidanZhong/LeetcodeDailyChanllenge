e9649deb27f5562d66de55bf396f61d121b84794
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-08-20 12:58:42 +0100

    Add 1386. Implement solution for Cinema Seat Allocation

--- code/1386_cinema_seat_allocation.py
+++ code/1386_cinema_seat_allocation.py
@@ -1,9 +1,38 @@
 # Problem: Cinema Seat Allocation
 # Topic: Greedy
 # Difficulty: Medium
+from collections import defaultdict
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        reserved = defaultdict(set)
+        for row, col in reservedSeats:
+            reserved[row].add(col)
+        ans = 2 * (n - len(reserved))
+        for cols in reserved.values():
+            available_seats = [1, 1, 1]
+            if 2 in cols or 3 in cols:
+                available_seats[0] = 0
+            if 4 in cols or 5 in cols:
+                available_seats[0] = 0
+                available_seats[1] = 0
+            if 6 in cols or 7 in cols:
+                available_seats[1] = 0
+                available_seats[2] = 0
+            if 8 in cols or 9 in cols:
+                available_seats[2] = 0
+
+            s = sum(available_seats)
+            if s == 1 or s == 0:
+                ans += s
+            elif available_seats == [1, 1, 0] or available_seats == [0, 1, 1]:
+                ans += 1
+            else:
+                ans += 2
+        return ans
+
+
+
+print(Solution().maxNumberOfFamilies(4, [[4, 3], [1, 4], [4, 6], [1, 7]]))
