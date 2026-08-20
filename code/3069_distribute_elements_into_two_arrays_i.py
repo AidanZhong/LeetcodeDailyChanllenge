e05953b76d0036ce441c91a637561e79b7bfded5
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-08-20 15:16:00 +0100

    Add 3069. Implement solution for Distribute Elements Into Two Arrays I

--- code/3069_distribute_elements_into_two_arrays_i.py
+++ code/3069_distribute_elements_into_two_arrays_i.py
@@ -1,9 +1,21 @@
 # Problem: Distribute Elements Into Two Arrays I
 # Topic: Array, Simulation
 # Difficulty: Easy
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def resultArray(self, nums: List[int]) -> List[int]:
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+
+        return arr1 + arr2
+
+
+print(Solution().resultArray([2, 1, 3]))
