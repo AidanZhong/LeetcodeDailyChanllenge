0881ca5d8b1a114ca8f3adf3b55797dcbf5b95ef
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-08-25 11:22:32 +0100

    3718

--- code/3718_smallest_missing_multiple_of_k.py
+++ code/3718_smallest_missing_multiple_of_k.py
@@ -1,9 +1,13 @@
 # Problem: Smallest Missing Multiple of K
 # Topic: Hashset
 # Difficulty: Easy
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        s = set(nums)
+        for i in range(1, max(nums) + 2):
+            if k * i not in s:
+                return k * i
+        return -1
