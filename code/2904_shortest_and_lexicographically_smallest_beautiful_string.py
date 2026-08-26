e49fda93a0a862a4f22be53878b9a6abc14a013d
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-08-26 09:51:46 +0100

    2904

--- code/2904_shortest_and_lexicographically_smallest_beautiful_string.py
+++ code/2904_shortest_and_lexicographically_smallest_beautiful_string.py
@@ -4,6 +4,24 @@
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        sub = ""
+        l, r = 0, 0
+        count_of_one = 0
+        while l <= r < len(s):
+            if s[r] == '1':
+                count_of_one += 1
+            if count_of_one == k:
+                while s[l] == '0':
+                    l += 1
+                temp_sub_string = s[l:r + 1]
+                if (sub == "" or r - l + 1 < len(sub) or
+                        r - l + 1 == len(sub) and temp_sub_string < sub):
+                    sub = temp_sub_string
+                count_of_one -= 1
+                l += 1
+            r += 1
+        return sub
+
+
+print(Solution().shortestBeautifulSubstring("100011001", 3))
