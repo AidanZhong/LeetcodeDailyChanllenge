ac57a78a4b33bc7cdf39fc73cc3a94378b87e4df
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-01 10:53:20 +0100

    3568 done

--- code/3568_minimum_moves_to_clean_the_classroom.py
+++ code/3568_minimum_moves_to_clean_the_classroom.py
@@ -1,9 +1,52 @@
 # Problem: Minimum Moves to Clean the Classroom
 # Topic: Simulation
 # Difficulty: Medium
+from collections import deque
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def minMoves(self, classroom: List[str], energy: int) -> int:
+        q = deque()
+        # find the starting point
+        n = len(classroom)
+        m = len(classroom[0])
+        start_point = -1, -1
+        litter_index = {}
+        for i in range(n):
+            for j in range(m):
+                if classroom[i][j] == 'S':
+                    start_point = i, j
+                elif classroom[i][j] == 'L':
+                    litter_index[(i, j)] = len(litter_index)
+        litter_count = len(litter_index)
+        if litter_count == 0:
+            return 0
+        full_mask = (1 << litter_count) - 1
+
+        sx, sy = start_point
+        q.append((0, sx, sy, energy, 0))
+        visited = {(sx, sy, energy, 0)}
+        while q:
+            moves, x, y, cur_energy, mask = q.popleft()
+            if cur_energy == 0:
+                continue
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and classroom[nx][ny] != 'X':
+                    n_energy = energy if classroom[nx][ny] == 'R' else cur_energy - 1
+                    n_mask = mask
+                    if (nx, ny) in litter_index:
+                        n_mask |= 1 << litter_index[(nx, ny)]
+                    if n_mask == full_mask:
+                        return moves + 1
+                    state = (nx, ny, n_energy, n_mask)
+                    if state not in visited:
+                        visited.add(state)
+                        q.append((moves + 1, nx, ny, n_energy, n_mask))
+        return -1
+
+
+# print(Solution().minMoves(["S.", "XL"], 2))
+# print(Solution().minMoves(classroom=["LS", "RL"], energy=4))
+print(Solution().minMoves(["SR"], 1))
--- code/3734_lexicographically_smallest_palindromic_permutation_greater_than_target.py
+++ code/3734_lexicographically_smallest_palindromic_permutation_greater_than_target.py
@@ -4,6 +4,56 @@
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def lexPalindromicPermutation(self, s: str, target: str) -> str:
+        letter_dict = [0] * 26
+        for i in s:
+            letter_dict[ord(i) - ord('a')] += 1
+
+        centra_char = ''
+        if len(s) % 2 == 1:
+            for i in range(26):
+                if letter_dict[i] % 2 == 1:
+                    centra_char = chr(i + ord('a'))
+                    letter_dict[i] -= 1
+            if not centra_char:
+                return ''
+
+        def could_form(cur):
+            # form the biggest string
+            max_str = ''
+            for x in range(25, -1, -1):
+                max_str += chr(x + ord('a')) * (letter_dict[x] // 2)
+            max_str = cur + max_str + centra_char + max_str[::-1] + cur[::-1]
+            return max_str > target
+
+        def form_smallest(cur):
+            smallest = ''
+            for x in range(26):
+                smallest += chr(i + ord('a')) * (letter_dict[x] // 2)
+            return cur + smallest + centra_char + smallest[::-1] + cur[::-1]
+
+        ans = ''
+        flag_of_greater = False
+        for idx, t in enumerate(target[:len(target) // 2]):
+            t_idx = ord(t) - ord('a')
+            if flag_of_greater:
+                return form_smallest(ans)
+            else:
+                if letter_dict[t_idx] >= 2 and could_form(ans):
+                    letter_dict[t_idx] -= 2
+                    ans += t
+                else:
+                    # find the next letter
+                    for offset in range(1, 26):
+                        next_char = t_idx + offset
+                        if next_char < 26 and letter_dict[next_char] > 0:
+                            break
+                    else:
+                        return ''
+                    letter_dict[next_char] -= 2
+                    ans += chr(next_char + ord('a'))
+                    flag_of_greater = True
+        return ans if flag_of_greater else ''
+
+
+print(Solution().lexPalindromicPermutation('baba', 'abba'))
