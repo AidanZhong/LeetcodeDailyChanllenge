c1b00d43f25f2ef63e5f9401d63ff8a557ad14a0
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-03 11:52:07 +0100

    3876

--- code/3876_construct_uniform_parity_array_ii.py
+++ code/3876_construct_uniform_parity_array_ii.py
@@ -1,9 +1,21 @@
 # Problem: Construct Uniform Parity Array II
 # Topic: Math
 # Difficulty: Medium
+import sys
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def uniformArray(self, nums1: list[int]) -> bool:
+        n = len(nums1)
+        is_odd = False
+        min_odd = sys.maxsize
+        min_even = sys.maxsize
+        for i in nums1:
+            if i % 2 == 1:
+                is_odd = True
+                min_odd = min(min_odd, i)
+            else:
+                min_even = min(min_even, i)
+        if not is_odd:
+            return True
+        return min_even > min_odd
