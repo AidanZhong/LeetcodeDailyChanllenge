72810cbe8d1a3807ea088c5da819129f5f952ca4
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-04 10:49:45 +0100

    3903

--- code/3903_smallest_stable_index_i.py
+++ code/3903_smallest_stable_index_i.py
@@ -4,6 +4,20 @@
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        max_i = [nums[0]]
+        for i in range(1, n):
+            max_i.append(max(max_i[-1], nums[i]))
+
+        min_i = [nums[-1]]
+        for i in range(n-2, -1, -1):
+            min_i.append(min(min_i[-1], nums[i]))
+        min_i.reverse()
+
+        for i in range(n):
+            if max_i[i] - min_i[i] <= k:
+                return i
+        return -1
+
+print(Solution().firstStableIndex([3,2,1], 1))  # Output:
