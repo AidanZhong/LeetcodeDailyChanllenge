9a9a0652bc21e54cce5e18d997723850ca345887
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-07 12:01:13 +0100

    940

--- code/940_distinct_subsequences_ii.py
+++ code/940_distinct_subsequences_ii.py
@@ -4,6 +4,17 @@
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def distinctSubseqII(self, s: str) -> int:
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1 # empty string
+        last_occurrence = {}
+        for i in range(1, len(s) + 1):
+            cur_char = s[i - 1]
+            dp[i] = (2 * dp[i - 1]) % (10**9 + 7)
+
+            if cur_char in last_occurrence:
+                dp[i] -= dp[last_occurrence[cur_char]]
+
+            last_occurrence[cur_char] = i - 1
+
+        return (dp[len(s)] - 1) % (10**9 + 7)  # subtract the empty subsequence
