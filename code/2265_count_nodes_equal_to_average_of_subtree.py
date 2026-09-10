44883648b9788841f5925292620cf1425dbed6bf
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-10 17:09:56 +0100

    2265

--- code/2265_count_nodes_equal_to_average_of_subtree.py
+++ code/2265_count_nodes_equal_to_average_of_subtree.py
@@ -2,8 +2,29 @@
 # Topic: DFS
 # Difficulty: Medium
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def averageOfSubtree(self, root: TreeNode) -> int:
+        count = 0
+
+        def sub_tree_sum_and_count(node):
+            if not node:
+                return (0, 0)
+            left_sum, left_count = sub_tree_sum_and_count(node.left)
+            right_sum, right_count = sub_tree_sum_and_count(node.right)
+            total_sum = left_sum + right_sum + node.val
+            total_count = left_count + right_count + 1
+            if total_sum // total_count == node.val:
+                nonlocal count
+                count += 1
+            return total_sum, total_count
+
+        sub_tree_sum_and_count(root)
+        return count
