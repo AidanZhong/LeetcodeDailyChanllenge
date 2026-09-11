c4a0dbbde3b1db0e4a91f91a592536d401aebfa0
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-11 10:42:32 +0100

    3483

--- code/3483_unique_3-digit_even_numbers.py
+++ code/3483_unique_3-digit_even_numbers.py
@@ -1,9 +1,16 @@
 # Problem: Unique 3-Digit Even Numbers
 # Topic: Math
 # Difficulty: Easy
+from collections import defaultdict
+from itertools import permutations
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def totalNumbers(self, digits: List[int]) -> int:
+        ans_set = set()
+        for perm in permutations(digits, 3):
+            num = int(''.join(map(str, perm)))
+            if 100 <= num <= 999 and num % 2 == 0:
+                ans_set.add(num)
+        return len(ans_set)
