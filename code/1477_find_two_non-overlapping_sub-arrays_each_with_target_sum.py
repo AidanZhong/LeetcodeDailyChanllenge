f77f9e1528a2bfbeb0e515edac322acbcd1e1180
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-17 11:05:19 +0100

    1477

--- code/1477_find_two_non-overlapping_sub-arrays_each_with_target_sum.py
+++ code/1477_find_two_non-overlapping_sub-arrays_each_with_target_sum.py
@@ -1,9 +1,38 @@
 # Problem: Find Two Non-overlapping Sub-arrays Each With Target Sum
-# Topic: Dynammic Programming
+# Topic: Sliding window, Prefix sum
 # Difficulty: Medium
+import sys
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def minSumOfLengths(self, arr: List[int], target: int) -> int:
+        pre_sum = [0]
+        for x in arr:
+            pre_sum.append(pre_sum[-1] + x)
+
+        n = len(arr)
+        l, r = 0, 0
+
+        best = [sys.maxsize] * n
+        ans = sys.maxsize
+        while l <= r < n:
+            if r > 0:
+                best[r] = min(best[r], best[r - 1])
+            if pre_sum[r + 1] - pre_sum[l] < target:
+                r += 1
+            elif pre_sum[r + 1] - pre_sum[l] > target:
+                l += 1
+                if l > r:
+                    r = l
+            else:
+                best[r] = min(best[r], r - l + 1)
+                if l > 0 and best[l - 1] != sys.maxsize:
+                    ans = min(ans, best[l - 1] + r - l + 1)
+                r += 1
+                l += 1
+
+        return ans if ans != sys.maxsize else -1
+
+
+print(Solution().minSumOfLengths([3, 2, 2, 4, 3], 3))
--- solutions/1477_find_two_non-overlapping_sub-arrays_each_with_target_sum.md
+++ solutions/1477_find_two_non-overlapping_sub-arrays_each_with_target_sum.md
@@ -1,7 +1,7 @@
 # 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
 
 - **Difficulty:** Medium
-- **Topic:** Dynammic Programming
+- **Topic:** Sliding window, Prefix sum
 
 [LeetCode](https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/)
 
