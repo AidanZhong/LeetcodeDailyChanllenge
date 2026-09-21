7e985bff05c6f641dcfddfc1dd0403bed180a9c3
AidanZhong <aidan.zhong@mosaicinsurance.com>
2026-09-21 13:35:54 +0100

    3524

--- code/3524_find_x_value_of_array_i.py
+++ code/3524_find_x_value_of_array_i.py
@@ -1,9 +1,33 @@
 # Problem: Find X Value of Array I
 # Topic: Dynamic programming
 # Difficulty: Medium
+from typing import List
 
 
 class Solution:
-    def solve(self):
-        # TODO: implement solution
-        pass
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        result = [0] * k
+
+        def dp(i):
+            # first i elements, return counts of subarrays ending at i - 1 by remainder
+            if i == 0:
+                return [0] * k
+            current_remainder = nums[i - 1] % k
+            previous_x_values = dp(i - 1)
+            new_x_values = [0] * k
+            new_x_values[current_remainder] = 1
+            for r in range(k):
+                new_remainder = (r * current_remainder) % k
+                new_x_values[new_remainder] += previous_x_values[r]
+            for r in range(k):
+                result[r] += new_x_values[r]
+            return new_x_values
+
+        dp(n)
+        return result
+
+
+print(Solution().resultArray([1, 2, 3, 4, 5], 3))
+print(Solution().resultArray([1, 2, 4, 8, 16, 32], 4))
+print(Solution().resultArray([1], 1))
